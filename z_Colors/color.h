0c363104861c1fc8292aa634237eae0f4ba1425d
Schwalm93 <79628051+Schwalm93@users.noreply.github.com>
2022-05-01 10:56:44 +0200

    Add files via upload

---
+++ z_Colors/color.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   color.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cschwalm <cschwalm@students.42wolfsburg.de +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/01 04:52:17 by cschwalm          #+#    #+#             */
+/*   Updated: 2022/05/01 04:55:43 by cschwalm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COLOR_H
+# define COLOR_H
+# include <stdio.h>
+
+# define ANSI_COLOR_RED		 	"\x1b[31m"
+# define RED					1
+# define ANSI_COLOR_GREEN		"\x1b[32m"
+# define GREEN					2
+# define ANSI_COLOR_YELLOW		"\x1b[33m"
+# define YELLOW					3
+# define ANSI_COLOR_BLUE		"\x1b[34m"
+# define BLUE					4
+# define ANSI_COLOR_MAGENTA		"\x1b[35m"
+# define MAGENTA				5
+# define ANSI_COLOR_CYAN		"\x1b[36m"
+# define CYAN					6
+# define ANSI_COLOR_RESET		"\x1b[0m"
+#endif
